bbc9cbcfbd5ee0841751b416af4f93929311beba
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2022-11-03 23:20:26 -0400

    Add Helper Func

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,6 +1,28 @@
 
 
- export const formatNumberByPercent = (total: number, percentage: number) =>{
+ export const formatNumberByPercent = (total: number, percentage?: number) =>{
+  if(typeof percentage === 'undefined'){
+    percentage = 100
+  }
   const numFormatter = Intl.NumberFormat('en', {notation: 'compact'});
-  return numFormatter.format((total / 100 ) * percentage)
-};
+  const result = numFormatter.format((total / 100 ) * percentage!);
+  if(result === '0'){
+    return "N/A"
+  }
+  return result;
+}
+
+// export function capitalizeFirstLetter(str : string) {
+//   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+// }
+
+export function capitalizeFirstLetter(str: any) 
+{
+  str = str.split(" ");
+
+  for (let i = 0, x = str.length; i < x; i++) {
+      str[i] = str[i][0].toUpperCase() + str[i].substr(1).toLowerCase();
+  }
+
+  return str.join(" ");
+}
