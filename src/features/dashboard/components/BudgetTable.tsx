787611b16fb84dbd1a65ebe8c38a943c9380e83e
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2022-12-26 19:07:55 -0500

    refactor: remove unecessary comment

--- src/features/dashboard/components/BudgetTable.tsx
+++ src/features/dashboard/components/BudgetTable.tsx
@@ -1,23 +1,26 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Card from '@mui/material/Card';
-import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
-import { Typography } from '@mui/material';
-import {selectBudget } from "../../cityData/cityDataSlice"
-import { useAppSelector } from '../../../app/hooks';
-import Grid from "@mui/material/Grid"
-import {formatNumberByPercent, capitalizeFirstLetter} from "../../../app/helpers"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Card from "@mui/material/Card";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import { Typography, useTheme } from "@mui/material";
+import { selectBudget } from "../../cityData/cityDataSlice";
+import { useAppSelector } from "../../../app/hooks";
+import Grid from "@mui/material/Grid";
+import {
+  formatNumberByPercent,
+  capitalizeFirstLetter,
+} from "../../../app/helpers";
 
 interface TablePaginationActionsProps {
   count: number;
@@ -25,29 +28,34 @@ interface TablePaginationActionsProps {
   rowsPerPage: number;
   onPageChange: (
     event: React.MouseEvent<HTMLButtonElement>,
-    newPage: number,
+    newPage: number
   ) => void;
 }
 
 function TablePaginationActions(props: TablePaginationActionsProps) {
-  
   const { count, page, rowsPerPage, onPageChange } = props;
 
   const handleFirstPageButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>,
+    event: React.MouseEvent<HTMLButtonElement>
   ) => {
     onPageChange(event, 0);
   };
 
-  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
     onPageChange(event, page - 1);
   };
 
-  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
     onPageChange(event, page + 1);
   };
 
-  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
     onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
   };
 
@@ -58,21 +66,21 @@ function TablePaginationActions(props: TablePaginationActionsProps) {
         disabled={page === 0}
         aria-label="first page"
       >
-       <FirstPageIcon />
+        <FirstPageIcon />
       </IconButton>
       <IconButton
         onClick={handleBackButtonClick}
         disabled={page === 0}
         aria-label="previous page"
       >
-         <KeyboardArrowLeft />
+        <KeyboardArrowLeft />
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page"
       >
-      <KeyboardArrowRight />
+        <KeyboardArrowRight />
       </IconButton>
       <IconButton
         onClick={handleLastPageButtonClick}
@@ -85,86 +93,78 @@ function TablePaginationActions(props: TablePaginationActionsProps) {
   );
 }
 
-
- function BudgetTable() {
+function BudgetTable() {
   const [page, setPage] = React.useState(0);
- 
+
   const rowsPerPage = 8;
 
   const budgetRows = useAppSelector(selectBudget);
 
-const rows = []
- 
-for (const [dept, amt] of Object.entries(budgetRows)) {
+  const rows = [];
 
- rows.push({ dept,  amt})
-}
-
+  for (const [dept, amt] of Object.entries(budgetRows)) {
+    rows.push({ dept, amt });
+  }
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
+    const mode = useTheme().palette.mode;
+
   const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number,
+    newPage: number
   ) => {
     setPage(newPage);
   };
 
-
-
   return (
-   <Grid item md={4}>
- 
-    <TableContainer 
-    component={Card}
-    elevation={0}
-    sx={{  border: "solid 1px gainsboro" }}
-    >
-    <Typography padding={2} variant='h6'>Budget</Typography>
-      <Table  aria-label="New York City Budget Breakdown" >
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row) => (
-            <TableRow key={row.dept}>
-              <TableCell component="th" scope="row">
-                {capitalizeFirstLetter(row.dept)}
-              </TableCell>
-              <TableCell  sx={{fontWeight: 700}} align="right">
-             {formatNumberByPercent(row.amt!,100)}
-              </TableCell>
+    <Grid item md={4}>
+      <TableContainer
+        component={Card}
+        elevation={0}
+        sx={{ border: mode === "dark" ? "" : "solid 1px gainsboro" }}
+      >
+        <Typography padding={2} variant="h6">
+          Budget
+        </Typography>
+        <Table aria-label="New York City Budget Breakdown">
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
+              <TableRow key={row.dept}>
+                <TableCell component="th" scope="row">
+                  {capitalizeFirstLetter(row.dept)}
+                </TableCell>
+                <TableCell sx={{ fontWeight: 700 }} align="right">
+                  {formatNumberByPercent(row.amt!, 100)}
+                </TableCell>
+              </TableRow>
+            ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                ActionsComponent={TablePaginationActions}
+              />
             </TableRow>
-          ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-          rowsPerPageOptions={[]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-               page={page}
-              // SelectProps={{
-              //   inputProps: {
-              //     'aria-label': 'rows per page',
-              //   },
-              //   native: true,
-              // }}
-               onPageChange={handleChangePage}
-               ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </Grid>
   );
 }
